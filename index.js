22eefd2e12b6e77dbbc288c6576ee81b2faf935f
sampath periasamy <sampathper2025@gmail.com>
2025-07-26 17:08:07 -0400

    commented out the poem portion of index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,29 +15,29 @@ const firebaseConfig = {
 };
 
 
-const firebaseApp = initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-async function submitPoem() {
-  const text = document.getElementById('poemInput').value.trim();
-  if (text === '') return alert('Please enter a poem');
-  await db.collection('poems').add({
-    content: text,
-    timestamp: firebase.firestore.FieldValue.serverTimestamp()
-  });
-  document.getElementById('poemInput').value = '';
-  loadLatestPoem();
-}
+// async function submitPoem() {
+//   const text = document.getElementById('poemInput').value.trim();
+//   if (text === '') return alert('Please enter a poem');
+//   await db.collection('poems').add({
+//     content: text,
+//     timestamp: firebase.firestore.FieldValue.serverTimestamp()
+//   });
+//   document.getElementById('poemInput').value = '';
+//   loadLatestPoem();
+// }
 
-async function loadLatestPoem() {
-  const poemsRef = db.collection('poems').orderBy('timestamp', 'desc').limit(1);
-  const snapshot = await poemsRef.get();
-  if (!snapshot.empty) {
-    const poem = snapshot.docs[0].data().content;
-    document.getElementById('poemDisplay').innerText = poem;
-  } else {
-    document.getElementById('poemDisplay').innerText = 'No poems yet.';
-  }
-}
+// async function loadLatestPoem() {
+//   const poemsRef = db.collection('poems').orderBy('timestamp', 'desc').limit(1);
+//   const snapshot = await poemsRef.get();
+//   if (!snapshot.empty) {
+//     const poem = snapshot.docs[0].data().content;
+//     document.getElementById('poemDisplay').innerText = poem;
+//   } else {
+//     document.getElementById('poemDisplay').innerText = 'No poems yet.';
+//   }
+// }
 
-loadLatestPoem();
+// loadLatestPoem();
