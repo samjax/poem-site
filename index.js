d1a997924087a887caaae26603adbf27473aa227
sampath periasamy <sampathper2025@gmail.com>
2025-07-27 12:14:05 -0400

    the CDN was incorrect, updated it now..removed firestore reference as well

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,7 +16,7 @@
 
   // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+// const db = getFirestore(app);
 
 // // Submit a new poem
 // export async function submitPoem() {
