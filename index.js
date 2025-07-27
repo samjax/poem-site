93f6a9a2cc8bf52d2adb0254d180b3364b91b58c
sampath periasamy <sampathper2025@gmail.com>
2025-07-27 12:30:48 -0400

    added required functions to import statement with ref to firestore

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+  import { getFirestore,collection,addDoc,serverTimestamp,getDocs,query,orderBy,limit } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
